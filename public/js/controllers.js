f617663c28ae16c53234489225eb32faee05de6c
姜德奇 <jiangdeqi@iscs.com.cn>
2016-08-18 19:27:35 +0800

    整理格式

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,48 +1,49 @@
 //index控制器
-app.controller('PollListCtrl', ['$scope',function($scope) {
+app.controller('PollListCtrl', ['$scope', function($scope) {
     $scope.alert = '欢迎光临，杭州';
 }]);
 
 
-app.controller('indexCtrl', ['$scope','$http',function($scope,$http) {
+app.controller('indexCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.name = '大奇山森林公园';
-    $scope.add = function(){
-        var myData = {userid: $scope.userid ,password: $scope.password};
-        
-       $http.post('/addUser',myData)
-        .success(function(data){
-            alert('添加成功');
-        });
+    $scope.add = function() {
+        var myData = {
+            userid: $scope.userid,
+            password: $scope.password
+        };
+
+        $http.post('/addUser', myData)
+            .success(function(data) {
+                alert('添加成功');
+            });
     }
 
-    $scope.tests = [  
-        {  
-        name: 'test1'  
-        }, {  
-        name: 'test2'  
-        }, {  
-        name: 'test3'  
-        }  
-    ];
+    $scope.tests = [{
+        name: 'test1'
+    }, {
+        name: 'test2'
+    }, {
+        name: 'test3'
+    }];
 
 }]);
 
 //list
 app.controller('ListCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.name = '姜德奇';
-    $http.post('/findUser','')
-        .success(function(data){
+    $http.post('/findUser', '')
+        .success(function(data) {
             $scope.list = data;
         });
 }]);
 
 //new
-app.controller('NewCtrl', ['$scope',function($scope) {
+app.controller('NewCtrl', ['$scope', function($scope) {
     $scope.name = '网仓三号。。';
 }]);
 
 //item
-app.controller('ItemCtrl', ['$scope',function($scope) {
+app.controller('ItemCtrl', ['$scope', function($scope) {
     $scope.name = '习近平';
 
 }]);
